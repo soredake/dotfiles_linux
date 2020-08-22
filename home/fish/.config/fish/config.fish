ac2bc035cb7bec0517fd393fc89c0e284470e999
soredake <fdsfgs@krutt.org>
2020-08-22 20:44:28 +0300

    dasdas

--- home/fish/.config/fish/config.fish
+++ home/fish/.config/fish/config.fish
@@ -4,15 +4,12 @@ if status --is-login
     curl https://git.io/fisher --create-dirs -sLo $__fish_config_dir/functions/fisher.fish
     fish -c fisher
   end
-  #for sh in /etc/profile.d/*.sh; test -r $sh && bass source $sh; end
+  for sh in /etc/profile.d/*.sh; test -r $sh && bass source $sh; end
   set -gx PATH $PATH $HOME/.local/bin
   echo Hi sempai~
 end
 set -gx EDITOR "code -w"
 
-# rclone alias
-# TODO: remove when https://github.com/rclone/rclone/issues/2697 is done
-#alias uploadd 'rclone sync (status is-interactive && echo -P) --transfers 6 --fast-list --delete-before'
 # Shorter
 alias cps 'rsync --archive --compress --progress --verbose --executability -h --update --delete'
 alias fd 'fdfind'
@@ -21,31 +18,26 @@ alias e 'code'
 alias exip 'curl -s https://ipecho.net/plain'
 alias jc 'journalctl'
 alias jcu 'journalctl --user'
-alias nvmestats 'sudo smartctl -A /dev/nvme0' # TODO: test partitionmanager
+alias nvmestats 'sudo smartctl -a /dev/nvme0; sudo nvme error-log /dev/nvme0'
 alias sc 'systemctl'
 alias scu 'systemctl --user'
-alias t_danet2 'telegram-desktop -many -workdir $HOME/.local/share/TelegramDesktop_danet2' # TODO: https://github.com/telegramdesktop/tdesktop/issues/4261#issuecomment-619537063
-alias t_danet3 'telegram-desktop -many -workdir $HOME/.local/share/TelegramDesktop_danet3' # TODO: https://github.com/telegramdesktop/tdesktop/issues/4261#issuecomment-619537063
-alias t_danet4 'telegram-desktop -many -workdir $HOME/.local/share/TelegramDesktop_danet4' # TODO: https://github.com/telegramdesktop/tdesktop/issues/4261#issuecomment-619537063
+alias t_danet4 'telegram-desktop -many -workdir $HOME/.local/share/TelegramDesktop_danet4'
 alias vts 'echo vitetris --connect (exip):27015 && vitetris -listen 27015'
 alias iaupload 'ia upload --no-derive --checksum --verify --retries 10 -H x-archive-keep-old-version:0'
+# TODO: waiting for telegram update
+alias t_danet2 'telegram-desktop -many -workdir $HOME/.local/share/TelegramDesktop_danet2'
+alias t_danet3 'telegram-desktop -many -workdir $HOME/.local/share/TelegramDesktop_danet3'
 
 # backup
 function backup
-  test ! -d /media/danet/Bigdisk/main && sleep 120
-  echo "===Local backup==="
-  cps $HOME/main /media/danet/Bigdisk
-  # TODO: https://github.com/rclone/rclone/issues/3683
-  rclone dedupe --dedupe-mode newest 50gbmega:/main
-  # upload
-  echo "===Cloud backup==="
-  rclone cleanup gdrive:/
+  echo "===Moving phone stuff from cloud to disk==="
   rclone move gdrive:/phone-stuff/ $HOME/main/unsorted
+  echo "===Cloud backup==="
   parallel -j 2 rclone sync $HOME/main ::: {gdrive,50gbmega}:/main
 end
 
 function updatecleanup
-  flatpak uninstall --unused # https://github.com/flatpak/flatpak/issues/2639
+  flatpak uninstall --unused # https://github.com/flatpak/flatpak/issues/2639 https://github.com/flatpak/flatpak/pull/3783
   ubuntu-security-status --unavailable
   sudo apt autoremove
 end
--- setup.sh
+++ setup.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
-sudo add-apt-repository -y ppa:berglh/pulseaudio-a2dp # TODO: see pulseaudio-modules-bt
+sudo add-apt-repository -y ppa:berglh/pulseaudio-a2dp
 sudo add-apt-repository -y ppa:kisak/kisak-mesa
-sudo add-apt-repository -y ppa:libretro/stable
-sudo add-apt-repository -y ppa:lutris-team/lutris # TODO: https://github.com/lutris/lutris/issues/2553
-sudo add-apt-repository -y ppa:cdemu/ppa # TODO: https://bugs.launchpad.net/cdemu/+bug/105452 https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=705409
+sudo add-apt-repository -y ppa:cdemu/ppa
 sudo add-apt-repository -y ppa:kubuntu-ppa/backports
-sudo add-apt-repository -y ppa:apandada1/foliate # TODO: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=945270
-sudo add-apt-repository -y ppa:jonaski/strawberry # TODO: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=913079
-sudo add-apt-repository -y ppa:samoilov-lex/retrogames # TODO: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=947399 https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=870143
-sudo add-apt-repository -y ppa:maxiberta/kwin-lowlatency # https://github.com/tildearrow/kwin-lowlatency/issues/56
-sudo add-apt-repository -y ppa:openmw/openmw
+sudo add-apt-repository -y ppa:samoilov-lex/retrogames
+# mpv in main repos compiled without vulkan support, sigh
+sudo add-apt-repository -y ppa:mc3man/mpv-tests
+# vmware https://itsfoss.com/install-vmware-player-ubuntu-1310/
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/ppa
 # wine
 wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
-# linux-kernel
+# linux-kernel for fsync
+# dkms modules need this
+# https://sourceforge.net/projects/xanmod/files/releases/lts/
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key add -
-# dosbox-staging
-sudo add-apt-repository -y ppa:feignint/dosbox-staging
+# syncthing https://packages.debian.org/sid/syncthing
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
 # hamachi
 sudo add-apt-repository -y ppa:webupd8team/haguichi
+# newer version from ppa until groovy arrives
+sudo add-apt-repository -y ppa:phoerious/keepassxc
+sudo add-apt-repository -y ppa:openmw/openmw
+sudo add-apt-repository -y ppa:alexlarsson/flatpak
+sudo add-apt-repository -y ppa:qbittorrent-team/qbittorrent-stable
+# groovy is supported
+sudo add-apt-repository -y ppa:libretro/stable
+sudo add-apt-repository -y ppa:lutris-team/lutris
+sudo add-apt-repository -y ppa:jonaski/strawberry
+sudo add-apt-repository -y ppa:apandada1/foliate
+sudo add-apt-repository -y ppa:maxiberta/kwin-lowlatency
+sudo add-apt-repository -y ppa:feignint/dosbox-staging
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
 sudo apt upgrade -y
 
@@ -28,6 +42,12 @@ sudo apt install -y ./multimc*.deb
 # TODO: https://github.com/rclone/rclone/issues/3980
 wget --content-disposition https://downloads.rclone.org/v1.52.2/rclone-v1.52.2-linux-amd64.deb
 sudo apt install -y ./rclone*.deb
+# syncplay https://github.com/Syncplay/syncplay/issues/344
+wget --content-disposition https://github.com/Syncplay/syncplay/releases/download/v1.6.5/syncplay_1.6.5.deb
+sudo apt install -y ./syncplay*.deb
+# macintosh.js
+wget --content-disposition https://github.com/felixrieseberg/macintosh.js/releases/download/v1.0.6/macintosh.js_1.0.6_amd64.deb
+sudo apt install -y ./macintosh*.deb
 # vscode
 wget --content-disposition 'https://go.microsoft.com/fwlink/?LinkID=760868'
 sudo apt install -y ./code*.deb
@@ -39,13 +59,14 @@ sudo apt install -y ./gb-studio*.deb
 
 packages=(
   # optdeps
+  plasma-browser-integration
   network-manager-openvpn
+  # vmware TODO: gcc-10 is not needed in groovy
+  gcc-10 build-essential
   # partitionmanager
   smartmontools # TODO: https://bugs.kde.org/show_bug.cgi?id=422877
   # dolphin
   dolphin-plugins
-  # yuzu
-  libboost-context1.71-dev
   # pulseaudio
   pulseaudio-module-gsettings # TODO: https://bugs.launchpad.net/ubuntu/+source/ubuntu-meta/+bug/1884932
   # boxtron
@@ -53,8 +74,6 @@ packages=(
   # not deps
   android-sdk-platform-tools
   bleachbit
-  caffeine
-  cataclysm-dda-sdl
   cheese
   citra
   colobot
@@ -66,24 +85,26 @@ packages=(
   gamemode # TODO: https://bugs.launchpad.net/ubuntu/+source/kubuntu-meta/+bug/1886315
   gcdemu
   gimp
+  fatrace
   git-cola
+  iotop
   haguichi
   htop
   keepassxc
   kubuntu-restricted-extras
-  linux-xanmod linux-libc-dev # TODO: https://www.phoronix.com/scan.php?page=news_item&px=Futex2-System-Call-RFC https://www.gamingonlinux.com/2020/06/linux-kernel-patch-sent-in-for-comments-to-help-gaming
+  linux-xanmod-lts
   lutris
   mpv
+  nvme-cli
   obs-studio
   openmw-launcher
   parallel
   ppa-purge
-  pulseaudio-modules-bt # TODO: https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/merge_requests/227 TODO: this pr have sbc-hq support
+  pulseaudio-modules-bt # TODO: https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/merge_requests/227
   python3-pip
   python3-venv # TODO: https://github.com/arsenetar/dupeguru/issues/484
   qbittorrent
   rclone
-  rename
   residualvm
   retroarch
   ripgrep
@@ -100,26 +121,31 @@ packages=(
   translate-shell
   vitetris
   winehq-staging
-  winetricks
   xclip
-  yarn
-  yuzu
+  yarnpkg
   zeal
+  yuzu
 )
 
-# bye
-sudo apt purge snapd
+# snaps
+sudo snap set system refresh.retain=2
+sudo snap set system refresh.timer=fri,9:00~21:00
+snap install bitpay open-syobon-action yuzu
 
 # Install my packages
 sudo apt install --install-recommends -y "${packages[@]}"
-sudo apt install --install-suggests lm-sensors
+sudo apt install --install-suggests -y lm-sensors
 
-# TODO: https://github.com/flathub/flathub/pull/1656
+# i need to manually install this
+sudo apt-mark hold linux-libc-dev
+
 # TODO: https://github.com/flathub/flathub/pull/1553
-flatpak install -y flathub com.spotify.Client org.jdownloader.JDownloader org.taisei_project.Taisei com.viber.Viber com.discordapp.Discord
-flatpak override --filesystem=home com.discordapp.Discord
+flatpak install -y flathub com.spotify.Client org.jdownloader.JDownloader org.taisei_project.Taisei com.viber.Viber com.discordapp.Discord com.github.vladimiry.ElectronMail
+flatpak install -y https://flathub.org/beta-repo/appstream/com.google.Chrome.flatpakref
+flatpak install -y https://flatpak.citra-emu.org/citra-canary.flatpakref
+flatpak override --filesystem=home com.discordapp.Discord # https://github.com/flathub/com.discordapp.Discord/issues/99
 pip=(
-  git+https://github.com/simons-public/protonfixes@master
+  git+https://github.com/simons-public/protonfixes.git
   git+https://github.com/vn-ki/anime-downloader.git
   internetarchive
   protontricks
@@ -127,42 +153,9 @@ pip=(
 )
 pip3 install -U "${pip[@]}"
 
-vspackages=(
-  EditorConfig.EditorConfig
-  HookyQR.beautify
-  Tyriar.sort-lines
-  anseki.vscode-color
-  bmalehorn.vscode-fish
-  bungcip.better-toml
-  chrislajoie.vscode-modelines
-  coolbear.systemd-unit-file
-  deerawan.vscode-dash
-  dpkristensen-garmin.udev-rules
-  eamodio.gitlens
-  file-icons.file-icons
-  geekidos.vdf
-  jaspernorth.vscode-pigments
-  jaydenlin.ctags-support
-  jeff-hykin.code-eol
-  jit-y.vscode-advanced-open-file
-  mads-hartmann.bash-ide-vscode
-  malmaud.tmux
-  mgmcdermott.vscode-language-babel
-  timonwong.shellcheck
-  ulthes.theme-firewatch
-  wayou.vscode-todo-highlight
-)
-
-parallel -j 1 code --install-extension ::: "${vspackages[@]}"
-
-# vmware https://itsfoss.com/install-vmware-player-ubuntu-1310/
-sudo apt install build-essential
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/ppa
-sudo apt install gcc-10
-
 # mpv scripts
 cd "$HOME/.config/mpv/scripts" || exit 1
-curl https://raw.githubusercontent.com/ElegantMonkey/mpv-webm/master/build/webm.lua -LO
+curl -LO https://raw.githubusercontent.com/ElegantMonkey/mpv-webm/master/build/webm.lua -LO https://raw.githubusercontent.com/haasn/gentoo-conf/xor/home/nand/.mpv/scripts/avail/skipchapters.lua
 
 # setup dofiles
 ../etc_cp/install.sh
@@ -170,7 +163,15 @@ curl https://raw.githubusercontent.com/ElegantMonkey/mpv-webm/master/build/webm.
 
 chsh -s /usr/bin/fish
 systemctl enable --now amdgpu
-systemctl --user enable --now backup.timer
+systemctl --user enable --now backup.timer syncthing
+
+# meh
+# https://wiki.archlinux.org/index.php/XDG_Autostart
+# https://bugs.kde.org/show_bug.cgi?id=413053#c7
+mkdir "$HOME/.config/autostart"
+cd "$HOME/.config/autostart" || exit 1
+sed -e '$aHidden=True' /etc/xdg/autostart/org.kde.discover.notifier.desktop > org.kde.discover.notifier.desktop
+sed -e '$aHidden=True' /etc/xdg/autostart/gcdemu.desktop > gcdemu.desktop
 
 # lutris: use system libretro cores
 # https://github.com/lutris/lutris/issues/2444
@@ -179,3 +180,8 @@ ln -sv "$HOME/.config/retroarch/cores" "$HOME/.local/share/lutris/runners/retroa
 # SBC HD
 # https://github.com/EHfive/pulseaudio-modules-bt/issues/63#issuecomment-613432583 https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/merge_requests/227 https://gitlab.freedesktop.org/pulseaudio/pulseaudio/-/issues/909
 sudo sed -i 's|load-module module-bluetooth-discover|load-module module-bluetooth-discover a2dp_config="sbc_min_bp=47 sbc_max_bp=47 sbc_freq=44k sbc_cmode=dual sbc_alloc=loudness sbc_sbands=8 sbc_blen=16"|g' /etc/pulse/default.pa
+
+# nvme settings
+rm "$HOME/.xsession-errors"
+touch "$HOME/.xsession-errors"
+sudo chattr +i .xsession-errors
