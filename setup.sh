838bde6a09c3bd3c5ca755c91d007eeeacd7d79c
soredake <5204968+soredake@users.noreply.github.com>
2021-02-04 09:28:49 +0200

    bcb

--- setup.sh
+++ setup.sh
@@ -58,6 +58,7 @@ packages=(
   steam
   stow
   syncthing
+  tor
   virtualbox virtualbox-guest-additions-iso
   vitetris
   winehq-staging
@@ -70,13 +71,11 @@ sudo dpkg --add-architecture i386
 sudo apt install --install-recommends -y "${packages[@]}"
 
 # flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub org.jdownloader.JDownloader org.taisei_project.Taisei com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft io.github.antimicrox.antimicrox com.uploadedlobster.peek com.neatdecisions.Detwinner net.rpcs3.RPCS3 org.telegram.desktop org.gtk.Gtk3theme.Breeze
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install -y flathub org.jdownloader.JDownloader org.taisei_project.Taisei com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft io.github.antimicrox.antimicrox com.uploadedlobster.peek com.neatdecisions.Detwinner net.rpcs3.RPCS3 org.telegram.desktop org.gtk.Gtk3theme.Breeze
 sudo flatpak override --filesystem=xdg-config/fontconfig:ro
 sudo flatpak override org.telegram.desktop --filesystem=host
-sudo flatpak override com.discordapp.Discord --filesystem=home
-# https://github.com/flatpak/flatpak/pull/4083
-# rpcs3 steam retroarch
+# rpcs3 steam retroarch needs this, https://github.com/flatpak/flatpak/pull/4083
 #sudo flatpak override --usb-device='input/*'
 sudo flatpak override --filesystem=/run/udev net.rpcs3.RPCS3
 
@@ -91,9 +90,9 @@ curl -LO https://raw.githubusercontent.com/ElegantMonkey/mpv-webm/master/build/w
 # general one-liners
 fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
 yarn set version berry
+systemctl --user mask --now pipewire.socket pipewire # https://bugs.launchpad.net/ubuntu/+source/pipewire/+bug/1897965
 pip3 install -U git+https://github.com/simons-public/protonfixes protontricks
 sudo tee -a /usr/share/sddm/scripts/Xsetup <<< "xmodmap /home/danet/git/dotfiles_home/home/xmodmap/.Xmodmap" # esc is broken
 
-# SBC XQ
-# https://github.com/EHfive/pulseaudio-modules-bt/issues/63#issuecomment-613432583
+# SBC XQ, https://github.com/EHfive/pulseaudio-modules-bt/issues/63#issuecomment-613432583
 sudo sed -i 's|load-module module-bluetooth-discover$|load-module module-bluetooth-discover a2dp_config="sbc_min_bp=47 sbc_max_bp=47 sbc_freq=44k sbc_cmode=dual sbc_alloc=loudness sbc_sbands=8 sbc_blen=16"|g' /etc/pulse/default.pa
