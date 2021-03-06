4d9c2c657b1f9a6c782b49e0afd1c499ea09cb03
soredake <5204968+soredake@users.noreply.github.com>
2021-03-06 21:14:53 +0200

    czxzdzcxzc

--- setup.sh
+++ setup.sh
@@ -3,11 +3,8 @@ sudo add-apt-repository -y ppa:libretro/stable
 sudo add-apt-repository -y ppa:berglh/pulseaudio-a2dp
 sudo add-apt-repository -y ppa:maxiberta/kwin-lowlatency
 sudo add-apt-repository -y ppa:cdemu/ppa
-sudo add-apt-repository -y ppa:kubuntu-ppa/backports
 sudo add-apt-repository -y ppa:kisak/kisak-mesa
-sudo add-apt-repository ppa:radeon-profile/radeon-profile
 wget -qO - https://deb.nodesource.com/setup_current.x | sudo -E bash -
-sudo dpkg-reconfigure code # re-enable repo after upgrade
 # https://wiki.winehq.org/Ubuntu https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=959707 https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=971032
 wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo add-apt-repository -y "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
@@ -18,7 +15,7 @@ sudo add-apt-repository -y "deb https://packages.azlux.fr/debian/ buster main"
 sudo apt upgrade -y
 
 pushd /tmp || exit 1
-wget --content-disposition 'https://go.microsoft.com/fwlink/?LinkID=760868' https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive_3.3.9_all.deb
+wget --content-disposition https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive_3.3.9_all.deb
 packages=(
   ./*.deb
   adb
@@ -50,43 +47,31 @@ packages=(
   steam
   stow
   syncplay
-  syncthing
   virtualbox
   vitetris
   winehq-staging exe-thumbnailer
   yarnpkg
   zeal
-  radeon-profile
 )
-# wine and steam need this
-sudo dpkg --add-architecture i386
 # Install my packages
 sudo apt install --install-recommends -y "${packages[@]}"
-
-# flatpak & snap
+sudo snap install code --classic
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install -y flathub org.jdownloader.JDownloader org.taisei_project.Taisei com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft com.uploadedlobster.peek com.neatdecisions.Detwinner net.rpcs3.RPCS3 org.telegram.desktop org.citra_emu.citra
+sudo flatpak install -y flathub org.citra_emu.citra org.jdownloader.JDownloader com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft com.uploadedlobster.peek com.neatdecisions.Detwinner net.rpcs3.RPCS3 org.telegram.desktop org.freefilesync.FreeFileSync
 sudo flatpak override --filesystem=xdg-config/fontconfig:ro # https://github.com/flatpak/flatpak/issues/3947
 sudo flatpak override org.telegram.desktop --filesystem=host # https://github.com/flathub/org.telegram.desktop/issues/23
-# rpcs3 steam retroarch and taisei needs this https://github.com/flatpak/flatpak/pull/4083
-sudo flatpak override --filesystem=/run/udev net.rpcs3.RPCS3
-
-# mpv scripts
-pushd "$HOME/.config/mpv/scripts" || exit 1
-curl -O https://raw.githubusercontent.com/ekisu/mpv-webm/master/build/webm.lua
+pip3 install -U git+https://github.com/simons-public/protonfixes protontricks
+fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher edc/bass rafaelrinaldi/pure'
+yarn set version berry
+aria2c -c -d "$HOME/.config/mpv/scripts" https://raw.githubusercontent.com/ekisu/mpv-webm/master/build/webm.lua
 
 # setup dofiles
-cd ~2 || exit 1
+popd || exit 1
 etc_cp/install.sh
 home/install.sh
 
 # general one-liners
-fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher edc/bass rafaelrinaldi/pure'
-yarn set version berry
 systemctl enable --now hdparm
-systemctl --user mask --now pipewire.socket pipewire # https://bugs.launchpad.net/ubuntu/+source/pipewire/+bug/1897965
-pip3 install -U git+https://github.com/simons-public/protonfixes protontricks
-sudo tee -a /usr/share/sddm/scripts/Xsetup <<< "xmodmap /home/danet/git/dotfiles_home/home/xmodmap/.Xmodmap" # esc is broken, https://bugs.kde.org/show_bug.cgi?id=410088
-
-# SBC XQ https://github.com/EHfive/pulseaudio-modules-bt/issues/63#issuecomment-613432583
-sudo sed -i 's|load-module module-bluetooth-discover$|load-module module-bluetooth-discover a2dp_config="sbc_min_bp=47 sbc_max_bp=47 sbc_freq=44k sbc_cmode=dual sbc_alloc=loudness sbc_sbands=8 sbc_blen=16"|g' /etc/pulse/default.pa
+systemctl --user mask --now pipewire.socket pipewire # https://bugs.launchpad.net/ubuntu/+source/pipewire/+bug/1897965, fixed in >=0.3.17 https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/427
+sudo tee -a /usr/share/sddm/scripts/Xsetup <<< "xmodmap /home/danet/git/dotfiles_home/home/xmodmap/.Xmodmap"
+sudo sed -i 's|load-module module-bluetooth-discover$|load-module module-bluetooth-discover a2dp_config="sbc_min_bp=47 sbc_max_bp=47 sbc_freq=44k sbc_cmode=dual sbc_alloc=loudness sbc_sbands=8 sbc_blen=16"|g' /etc/pulse/default.pa # SBC XQ https://github.com/EHfive/pulseaudio-modules-bt/issues/63#issuecomment-613432583
