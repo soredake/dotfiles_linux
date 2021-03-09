2f472f897b723635fe093ba1cd8f57500a79c4f7
soredake <5204968+soredake@users.noreply.github.com>
2021-03-09 20:33:08 +0200

    вап

--- setup.sh
+++ setup.sh
@@ -4,8 +4,7 @@ sudo add-apt-repository -y ppa:berglh/pulseaudio-a2dp
 sudo add-apt-repository -y ppa:maxiberta/kwin-lowlatency
 sudo add-apt-repository -y ppa:cdemu/ppa
 sudo add-apt-repository -y ppa:kisak/kisak-mesa
-wget -qO - https://deb.nodesource.com/setup_current.x | sudo -E bash -
-# https://wiki.winehq.org/Ubuntu https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=959707 https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=971032
+# https://wiki.winehq.org/Ubuntu
 wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo add-apt-repository -y "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
 # rclone with mega backend https://github.com/rclone/rclone/issues/3980
@@ -16,53 +15,15 @@ sudo apt upgrade -y
 
 pushd /tmp || exit 1
 wget --content-disposition https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive_3.3.9_all.deb
-packages=(
-  ./*.deb
-  adb
-  bleachbit
-  chntpw
-  dolphin-plugins
-  earlyoom
-  filelight
-  fish
-  gamemode
-  gcdemu
-  gimp
-  internetarchive
-  keepassxc
-  lm-sensors
-  lutris
-  mpv
-  network-manager-openvpn
-  plasma-discover-backend-flatpak
-  ppa-purge
-  pulseaudio-modules-bt
-  python3-pip
-  qbittorrent
-  qdirstat
-  rclone-browser
-  retroarch
-  safeeyes
-  seahorse
-  steam
-  stow
-  syncplay
-  virtualbox
-  vitetris
-  winehq-staging exe-thumbnailer
-  yarnpkg
-  zeal
-)
 # Install my packages
-sudo apt install --install-recommends -y "${packages[@]}"
+sudo apt install --install-recommends -y ./*.deb adb bleachbit chntpw dolphin-plugins earlyoom filelight fish gamemode gcdemu gimp internetarchive keepassxc lm-sensors lutris mpv network-manager-openvpn plasma-discover-backend-flatpak ppa-purge pulseaudio-modules-b python3-pip qbittorrent qdirstat rclone-browser retroarch safeeyes seahorse steam stow syncplay virtualbox vitetris winehq-staging exe-thumbnailer
 sudo snap install code --classic
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install -y flathub org.citra_emu.citra org.jdownloader.JDownloader com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft com.uploadedlobster.peek com.neatdecisions.Detwinner net.rpcs3.RPCS3 org.telegram.desktop org.freefilesync.FreeFileSync
+sudo flatpak install -y flathub org.citra_emu.citra org.jdownloader.JDownloader com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft com.neatdecisions.Detwinner net.rpcs3.RPCS3 org.telegram.desktop org.freefilesync.FreeFileSync
 sudo flatpak override --filesystem=xdg-config/fontconfig:ro # https://github.com/flatpak/flatpak/issues/3947
 sudo flatpak override org.telegram.desktop --filesystem=host # https://github.com/flathub/org.telegram.desktop/issues/23
 pip3 install -U git+https://github.com/simons-public/protonfixes protontricks
-fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher edc/bass rafaelrinaldi/pure'
-yarn set version berry
+fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher rafaelrinaldi/pure'
 aria2c -c -d "$HOME/.config/mpv/scripts" https://raw.githubusercontent.com/ekisu/mpv-webm/master/build/webm.lua
 
 # setup dofiles
