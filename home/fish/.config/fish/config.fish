5b3efe8923825729aca7c519490e2ed2fb609702
soredake <5204968+soredake@users.noreply.github.com>
2021-03-23 18:09:06 +0200

    wdrtsdfsdfsdf

--- home/fish/.config/fish/config.fish
+++ home/fish/.config/fish/config.fish
@@ -1,4 +1,4 @@
-alias e 'code'
+#alias e 'code'
 alias exip 'curl -s https://ipecho.net/plain'
 alias g 'git'
 alias iaupload 'ia upload --checksum --verify --retries 10 -H x-archive-keep-old-version:0' # TODO: replace x-archive-keep-old-version with --no-backup after upgrading to 21.04
--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 sudo add-apt-repository -y ppa:libretro/stable
 sudo add-apt-repository -y ppa:berglh/pulseaudio-a2dp
 sudo add-apt-repository -y ppa:maxiberta/kwin-lowlatency
-sudo add-apt-repository -y ppa:cdemu/ppa
 sudo add-apt-repository -y ppa:kisak/kisak-mesa
+sudo add-apt-repository -y ppa:slgobinath/safeeyes
 # https://wiki.winehq.org/Ubuntu
 wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
 sudo add-apt-repository -y "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -cs) main"
@@ -16,14 +16,14 @@ sudo apt upgrade -y
 pushd /tmp || exit 1
 wget --content-disposition https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive_3.3.9_all.deb
 # Install my packages
-sudo apt install --install-recommends -y ./*.deb adb bleachbit chntpw dolphin-plugins earlyoom filelight fish gamemode gcdemu gimp internetarchive keepassxc lm-sensors lutris mpv network-manager-openvpn plasma-discover-backend-flatpak ppa-purge pulseaudio-modules-b python3-pip qbittorrent qdirstat rclone-browser retroarch safeeyes seahorse steam stow syncplay virtualbox vitetris winehq-staging exe-thumbnailer
+sudo apt install --install-recommends -y ./*.deb adb bleachbit chntpw dolphin-plugins earlyoom filelight fish gamemode gimp internetarchive keepassxc lm-sensors lutris mpv network-manager-openvpn plasma-discover-backend-flatpak ppa-purge pulseaudio-modules-bt python3-pip qbittorrent qdirstat rclone-browser retroarch safeeyes seahorse steam stow syncplay virtualbox vitetris winehq-staging exe-thumbnailer simplescreenrecorder
 sudo snap install code --classic
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install -y flathub org.citra_emu.citra org.jdownloader.JDownloader com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft com.neatdecisions.Detwinner net.rpcs3.RPCS3 org.telegram.desktop org.freefilesync.FreeFileSync
+sudo flatpak install -y flathub org.jdownloader.JDownloader com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft net.rpcs3.RPCS3 org.telegram.desktop org.freefilesync.FreeFileSync
 sudo flatpak override --filesystem=xdg-config/fontconfig:ro # https://github.com/flatpak/flatpak/issues/3947
 sudo flatpak override org.telegram.desktop --filesystem=host # https://github.com/flathub/org.telegram.desktop/issues/23
 pip3 install -U git+https://github.com/simons-public/protonfixes protontricks
-fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher rafaelrinaldi/pure'
+fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher pure-fish/pure'
 aria2c -c -d "$HOME/.config/mpv/scripts" https://raw.githubusercontent.com/ekisu/mpv-webm/master/build/webm.lua
 
 # setup dofiles
@@ -31,8 +31,8 @@ popd || exit 1
 etc_cp/install.sh
 home/install.sh
 
-# general one-liners
+# general settings
 systemctl enable --now hdparm
-systemctl --user mask --now pipewire.socket pipewire # https://bugs.launchpad.net/ubuntu/+source/pipewire/+bug/1897965, fixed in >=0.3.17 https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/427
+systemctl --user mask --now pipewire.socket pipewire # fixed in >=0.3.17
 sudo tee -a /usr/share/sddm/scripts/Xsetup <<< "xmodmap /home/danet/git/dotfiles_home/home/xmodmap/.Xmodmap"
 sudo sed -i 's|load-module module-bluetooth-discover$|load-module module-bluetooth-discover a2dp_config="sbc_min_bp=47 sbc_max_bp=47 sbc_freq=44k sbc_cmode=dual sbc_alloc=loudness sbc_sbands=8 sbc_blen=16"|g' /etc/pulse/default.pa # SBC XQ https://github.com/EHfive/pulseaudio-modules-bt/issues/63#issuecomment-613432583
