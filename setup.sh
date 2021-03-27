b80034ad05682df236c9219fe60b2693ee7006ae
soredake <5204968+soredake@users.noreply.github.com>
2021-03-27 19:56:58 +0200

    fdsf

--- setup.sh
+++ setup.sh
@@ -16,7 +16,8 @@ sudo apt upgrade -y
 pushd /tmp || exit 1
 wget --content-disposition https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive_3.3.9_all.deb
 # Install my packages
-sudo apt install --install-recommends -y ./*.deb adb bleachbit chntpw dolphin-plugins earlyoom filelight fish gamemode gimp internetarchive keepassxc lm-sensors lutris mpv network-manager-openvpn plasma-discover-backend-flatpak ppa-purge pulseaudio-modules-bt python3-pip qbittorrent qdirstat rclone-browser retroarch safeeyes seahorse steam stow syncplay virtualbox vitetris winehq-staging exe-thumbnailer simplescreenrecorder
+sudo apt install --install-recommends -y ./*.deb adb aria2 bleachbit chntpw dolphin-plugins earlyoom filelight fish gamemode gimp internetarchive keepassxc lm-sensors lutris mpv network-manager-openvpn plasma-discover-backend-flatpak ppa-purge pulseaudio-modules-bt python3-pip qbittorrent qdirstat rclone-browser retroarch safeeyes seahorse steam stow syncplay virtualbox vitetris winehq-staging exe-thumbnailer simplescreenrecorder
+sudo apt install `check-language-support`
 sudo snap install code --classic
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install -y flathub org.jdownloader.JDownloader com.viber.Viber com.github.ztefn.haguichi com.spotify.Client com.discordapp.Discord com.github.micahflee.torbrowser-launcher com.mojang.Minecraft net.rpcs3.RPCS3 org.telegram.desktop org.freefilesync.FreeFileSync
@@ -32,7 +33,7 @@ etc_cp/install.sh
 home/install.sh
 
 # general settings
-systemctl enable --now hdparm
+#systemctl enable --now hdparm
 systemctl --user mask --now pipewire.socket pipewire # fixed in >=0.3.17
 sudo tee -a /usr/share/sddm/scripts/Xsetup <<< "xmodmap /home/danet/git/dotfiles_home/home/xmodmap/.Xmodmap"
 sudo sed -i 's|load-module module-bluetooth-discover$|load-module module-bluetooth-discover a2dp_config="sbc_min_bp=47 sbc_max_bp=47 sbc_freq=44k sbc_cmode=dual sbc_alloc=loudness sbc_sbands=8 sbc_blen=16"|g' /etc/pulse/default.pa # SBC XQ https://github.com/EHfive/pulseaudio-modules-bt/issues/63#issuecomment-613432583
