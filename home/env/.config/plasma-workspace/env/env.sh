a3b9a97a0f96969cf969d71d4df611f37ca44b6f
soredake <5204968+soredake@users.noreply.github.com>
2021-09-02 14:56:21 +0300

    Update env.sh

--- home/env/.config/plasma-workspace/env/env.sh
+++ home/env/.config/plasma-workspace/env/env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export WINEESYNC=1
+export GTK_USE_PORTAL=1
 # ipega sdl bindings https://github.com/libsdl-org/SDL/pull/4060
 #export SDL_GAMECONTROLLERCONFIG="05000000491900000204000000000000,Ipega PG-9087S - Bluetooth Gamepad,a:b0,b:b1,y:b4,x:b3,start:b11,back:b10,leftstick:b13,rightstick:b14,leftshoulder:b6,rightshoulder:b7,dpup:h0.1,dpleft:h0.8,dpdown:h0.4,dpright:h0.2,leftx:a0,lefty:a1,rightx:a2,righty:a3,lefttrigger:b8,righttrigger:b9,platform:Linux,"
 #export PLASMA_USE_QT_SCALING=1
