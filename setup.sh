48a1f70db56d6dcaaf9100ab9a697474bd4eba95
soredake <5204968+soredake@users.noreply.github.com>
2021-09-07 20:39:42 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo add-apt-repository -y ppa:libretro/stable
-#sudo add-apt-repository -y ppa:maxiberta/kwin-lowlatency
 sudo add-apt-repository -y ppa:kisak/kisak-mesa
 sudo add-apt-repository -y ppa:slgobinath/safeeyes
 # https://wiki.winehq.org/Ubuntu
@@ -15,7 +14,7 @@ sudo apt upgrade -y
 pushd /tmp || exit 1
 wget --content-disposition https://www.vpn.net/installers/logmein-hamachi_2.1.0.203-1_amd64.deb https://www.thefanclub.co.za/sites/default/files/public/overgrive/overgrive_3.3.9_all.deb
 # Install my packages
-sudo apt install --install-recommends -y ./*.deb adb aria2 bleachbit chntpw dolphin-plugins earlyoom filelight fish gamemode gimp internetarchive lm-sensors lutris mpv network-manager-openvpn plasma-discover-backend-flatpak ppa-purge python3-pip qdirstat rclone-browser retroarch safeeyes seahorse steam stow syncplay virtualbox vitetris winehq-staging icoutils simplescreenrecorder trash-cli smartcontrol
+sudo apt install --install-recommends -y ./*.deb adb aria2 bleachbit chntpw dolphin-plugins earlyoom fish gamemode gimp internetarchive lm-sensors lutris mpv network-manager-openvpn plasma-discover-backend-flatpak ppa-purge python3-pip qdirstat rclone-browser retroarch safeeyes seahorse steam stow syncplay virtualbox vitetris winehq-staging icoutils simplescreenrecorder trash-cli smartcontrol
 sudo apt install `check-language-support`
 sudo snap install code --classic
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -23,7 +22,7 @@ sudo flatpak install -y flathub org.jdownloader.JDownloader com.viber.Viber com.
 sudo flatpak override --filesystem=xdg-config/fontconfig:ro # https://github.com/flatpak/flatpak/issues/3947
 #sudo flatpak override org.telegram.desktop --filesystem=host # https://github.com/flathub/org.telegram.desktop/issues/23
 sudo flatpak override org.jdownloader.JDownloader --filesystem=host
-pip3 install -U git+https://github.com/simons-public/protonfixes protontricks
+pip install -U git+https://github.com/simons-public/protonfixes protontricks
 fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher pure-fish/pure'
 aria2c -c -d "$HOME/.config/mpv/scripts" https://github.com/ekisu/mpv-webm/releases/download/latest/webm.lua
 
