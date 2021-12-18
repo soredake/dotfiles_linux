1225ec2481afe8f577d133311ca6d9fd532ed172
soredake <5204968+soredake@users.noreply.github.com>
2021-12-18 19:44:20 +0200

    not needed

--- home/fish/.config/fish/config.fish
+++ home/fish/.config/fish/config.fish
@@ -1,7 +1,4 @@
 #alias e 'code'
-#alias exip 'curl -s https://ipecho.net/plain'
 alias g 'git'
 alias iaupload 'ia upload --checksum --verify --retries 10 -H x-archive-keep-old-version:0'
-#alias nvmestats 'sudo smartctl -A /dev/nvme0'
-#echo vitetris --connect (exip):27015 && 
 alias vts 'vitetris -listen 27015'
