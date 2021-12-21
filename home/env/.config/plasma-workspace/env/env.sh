6fe1f361fb9f1602f159c285c71b57863ca0ea75
soredake <5204968+soredake@users.noreply.github.com>
2021-12-21 09:36:29 +0200

    Update env.sh

--- home/env/.config/plasma-workspace/env/env.sh
+++ home/env/.config/plasma-workspace/env/env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export WINEESYNC=1 # will not be needed once FUTEX2 will merged to wine
 export GTK_USE_PORTAL=1 # it's here forever i guess, https://bugzilla.mozilla.org/show_bug.cgi?id=1694445 https://invent.kde.org/neon/neon/settings/-/blob/Neon/unstable/usr/sbin/firefox https://bugs.launchpad.net/ubuntu/+source/kubuntu-settings/+bug/1910852
-#export PLASMA_USE_QT_SCALING=1
+#export PLASMA_USE_QT_SCALING=1 # https://bugs.kde.org/show_bug.cgi?id=356446
