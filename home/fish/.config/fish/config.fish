55388816ec8497906c38d335c634b87c961af8d5
soredake <5204968+soredake@users.noreply.github.com>
2022-01-18 11:15:43 +0200

    Update config.fish

--- home/fish/.config/fish/config.fish
+++ home/fish/.config/fish/config.fish
@@ -1,5 +1,5 @@
 #alias e 'code'
 alias g 'git'
 alias iaupload 'ia upload --checksum --verify --retries 10 --no-backup'
-alias iauploadf 'ia upload --checksum --verify --retries 10 --no-backup'
+alias iauploadf 'ia upload --verify --retries 10 --no-backup'
 alias vts 'vitetris -listen 27015'
