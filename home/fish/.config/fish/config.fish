0d1109925f102177ad90ff26e81327bda8e23885
soredake <5204968+soredake@users.noreply.github.com>
2023-07-03 11:26:30 +0300

    Update config.fish

--- home/fish/.config/fish/config.fish
+++ home/fish/.config/fish/config.fish
@@ -4,4 +4,5 @@ alias iauploadcheckderive 'ia upload --checksum --verify --retries 50 --no-backu
 alias iauploadfastderive 'ia upload --verify --retries 50 --no-backup'
 alias iauploadcheck 'ia upload --checksum --verify --retries 50 --no-backup --no-derive'
 alias iauploadfast 'ia upload --verify --retries 50 --no-backup --no-derive'
+alias iauploadveryfast 'ia upload --retries 50 --no-backup --no-derive'
 alias vts 'vitetris -listen 27015'
